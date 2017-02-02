a4bf0b683e8367360ece9c456a6a0b5e3b55b426
Jonathan <jmans25@gmail.com>
2017-02-01 23:14:02 -0500

    fasdfsdafa

--- uploadtogit.sh
+++ uploadtogit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #this scripts uploads the entire repo to github in one click!
-echo -e "Enter a commit message"
+echo -e "Enter a commit message:"
 read comment
 git add .
 git commit -a -m $comment
