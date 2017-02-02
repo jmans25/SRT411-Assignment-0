bde116631c430dcddb8b39be80c5e414c0aec115
Jonathan <jmans25@gmail.com>
2017-02-01 23:11:05 -0500

    another test

---
+++ uploadtogit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#this scripts uploads the entire repo to github in one click!
+echo -e "Enter a commit message >"
+read comment
+git add .
+git commit -a -m $comment
+git push -u origin master
+echo -e "Success!"
+sleep 5
